245fe10ae7a7020bab431c99bc7bb09f4d705793
Tarek1003 <mo.tarek1003@gmail.com>
2026-08-19 10:56:50 +0300

    about-me

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+
+const About = () => {
+  return (
+    <main
+      className='relative min-h-screen overflow-hidden bg-zinc-900 px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20 py-30'
+      id='about-us'>
+      <div className='text-center lg:text-left'>
+        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-gray-400 border-blue-400 pb-5 border-b-2 w-fit mx-auto '>
+          About-Me
+        </h1>
+        <p className='pt-2.5  text-white text-xs sm:text-sm md:text-base text-center'>
+          I am a third-year Pharmacy student at Beni-Suef University with a
+          growing passion for learning and development. I have developed a
+          strong interest in learning design and facilitation. My journey with
+          EPSF, especially within the Training Department, allowed me to work on
+          designing training kits, creating learning activities, and
+          facilitating sessions. These experiences helped me build practical
+          skills in learner engagement, content development, and delivering
+          meaningful learning experiences.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default About
