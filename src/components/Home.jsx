dd23fd61ba89e73cda0dae42b835e58d5fc74898
Tarek1003 <mo.tarek1003@gmail.com>
2026-08-19 08:32:15 +0300

    media query adjuted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import { BackgroundBeams } from "./ui/background-beams"
 import profile from "../assets/mariam.png"
 const Home = () => {
   return (
-    <main className='relative min-h-screen overflow-hidden bg-black px-24'>
+    <main className='relative min-h-screen overflow-hidden bg-black px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20'>
       <BackgroundBeams />
 
-      <div className='relative z-10 flex sm:flex-col-reverse md:flex-row  min-h-screen items-center justify-center gap-20'>
-        <div>
-          <h1 className='text-5xl font-bold text-blue-400'>Mariam Adel</h1>
-          <p className='text-2xl text-gray-400'>Instructional designer</p>
-          <p className='pt-2.5 text-2xl text-white'>
+      <div className='relative z-10 flex sm:flex-col-reverse lg:flex-row  min-h-screen items-center justify-center gap-5 lg:gap-70'>
+        <div className="sm:text-center lg:text-left">
+          <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-blue-400'>Mariam Adel</h1>
+          <p className='text-sm sm:text-base md:text-xl lg:text-xl  text-gray-400'>Instructional designer</p>
+          <p className='pt-2.5  text-white text-xs sm:text-sm md:text-base'>
             passionate about creating engaging and impactful learning
             experiences.
           </p>
