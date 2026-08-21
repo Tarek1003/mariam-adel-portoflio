440eaba5a83f169f293baa7c3de3c47672cbcefd
Tarek1003 <mo.tarek1003@gmail.com>
2026-08-21 11:43:19 +0300

    corroction

--- src/components/data/CarosulImages.js
+++ src/components/data/CarosulImages.js
@@ -1,10 +1,8 @@
-import photo1 from "../../assets/IMG_4476.JPG" 
-import photo2 from "../../assets/5a8b34c6-8635-4b3f-8b88-36455bc4266c.JPG"
-import photo3 from "../../assets/IMG_4493.JPG"
-import photo4 from "../../assets/IMG_6643.JPG"
-import photo5 from "../../assets/IMG_6645.JPG"
-import photo6 from "../../assets/IMG_6743.JPG"
+import photo1 from "../../assets/IMG_4476.jpg";
+import photo2 from "../../assets/5a8b34c6-8635-4b3f-8b88-36455bc4266c.jpg";
+import photo3 from "../../assets/IMG_4493.jpg";
+import photo4 from "../../assets/IMG_6643.jpg";
+import photo5 from "../../assets/IMG_6645.jpg";
+import photo6 from "../../assets/IMG_6743.jpg";
 
-export const carosulImages = [photo1, photo2,photo3,photo4,photo5,photo6  ]
-
-
+export const carosulImages = [photo1, photo2, photo3, photo4, photo5, photo6];
