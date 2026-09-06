7dd005fa1260d85c5b70135cc10831103fe918a5
Tarek1003 <mo.tarek1003@gmail.com>
2026-09-07 01:37:12 +0300

    final

--- src/components/data/CarosulImages.js
+++ src/components/data/CarosulImages.js
@@ -4,7 +4,17 @@ import photo3 from "../../assets/IMG_4493.jpg";
 import photo4 from "../../assets/IMG_6643.jpg";
 import photo5 from "../../assets/IMG_6645.jpg";
 import photo6 from "../../assets/IMG_6743.jpg";
-import photo7 from "../../assets/2.JPG"; 
-import photo8 from "../../assets/WhatsApp Image 2026-09-06 at 6.27.46 PM.jpeg"
-import photo9 from "../../assets/4.JPG"
-export const carosulImages = [photo1, photo2, photo3, photo4, photo5, photo6, photo8, photo7,photo9 ];
+import photo7 from "../../assets/2.JPG";
+import photo8 from "../../assets/WhatsApp Image 2026-09-06 at 6.27.46 PM.jpeg";
+import photo9 from "../../assets/4.JPG";
+export const carosulImages = [
+  photo4,
+  photo7,
+  photo9,
+  photo3,
+  photo1,
+  photo2,
+  photo5,
+  photo6,
+  photo8,
+];
